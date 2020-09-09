75b684634cab8c5f08a84d5c5084ae601fc43658
Mike Emmons <memmons18@gmail.com>
2020-09-09 08:52:45 -0400

    Updates

---
+++ precache-manifest.3e214fb00575b327682c3e78e96db229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3e372e05f9d3a01da028b553ea8702",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "b3350234b15ac04749e0",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2d58a49acaaf296ffb4",
+    "url": "/myapp/static/js/2.d44531b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myapp/static/js/2.d44531b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3350234b15ac04749e0",
+    "url": "/myapp/static/js/main.814903ee.chunk.js"
+  },
+  {
+    "revision": "805e7b8f7d167f37790c",
+    "url": "/myapp/static/js/runtime-main.e11a3646.js"
+  }
+]);
